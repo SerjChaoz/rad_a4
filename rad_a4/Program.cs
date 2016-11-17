96c118afea5a7c0fb14769df6d07b4c9b3a89a1c
chaoz <chaoz@DESKTOP-76D6VQM>
2016-11-17 17:59:00 -0400

    start

--- rad_a4/Program.cs
+++ rad_a4/Program.cs
@@ -16,7 +16,7 @@ namespace rad_a4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SplashScreen());
         }
     }
 }
